19b76ed0052b187c95982b4dc90ad8198703849f
PhishLex <16486206+matheesha@users.noreply.github.com>
2023-03-15 01:51:09 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 import requests
 import json
 import urllib.parse
+from google.appengine.api import urlfetch
 
 def predict(url, results):
   encodedurl = urllib.parse.quote_plus(url)
   apivurl = "https://endpoint.apivoid.com/urlrep/v1/pay-as-you-go/?key=d9bc1934f0f6e78ba810027df7adbc53fe360d73&url=" + encodedurl
   payload={}
   headers = {}
-  response = requests.request("GET", apivurl, headers=headers, data=payload)
-
-  res = json.loads(response.text)
-  detection_count = res['data']['report']['domain_blacklist']['detections']
-  results['detection_count'] = detection_count
-  risk_score = res['data']['report']['risk_score']['result']
-  results['risk_score'] = risk_score
-  results['is_shortner'] = res['data']['report']['site_category']['is_url_shortener']
-
-  if(detection_count > 0 or risk_score > 30): 
-    results['is_phishing'] = True
-  else:
-     results['is_phishing'] = False
-  return results
+  #response = requests.request("GET", apivurl, headers=headers, data=payload)
+  api_response = urlfetch.fetch(apivurl)
+  res = json.loads(api_response.text)
+  return res
 
 
 from flask import Flask
